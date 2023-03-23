0222a98c262f9b0e6da3e61e75e75fde18f78e05
mertbkts <merttbektas@hotmail.com.tr>
2023-03-23 11:47:21 +0200

    client

---
+++ client.py
@@ -0,0 +1,19 @@
+import grpc
+from datetime import datetime
+from datetime import timedelta
+import sys
+import time
+import threading
+
+
+def run():
+    value = int(input("Enter the value to calculate: "))
+    with grpc.insecure_channel('localhost:50051') as channel:
+
+        client = DateTimeClient(channel)
+
+        print(value)
+		
+
+if __name__ == '__main__':
+    run()
